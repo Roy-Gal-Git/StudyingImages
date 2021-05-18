6d1e1cf2eb53e985a926089e860fd53a4393c1f0
Roy-Gal-Git <self.roygal@gmail.com>
2021-05-19 00:42:57 +0300

    commit

--- JPEGtoPNGconverter.py
+++ JPEGtoPNGconverter.py
@@ -1,13 +1,29 @@
-import sys
 import os
 from PIL import Image
+from termcolor import cprint
 
 
 def print_enumerated_list(lst):
     counter = 0
+    cprint("Directories:", "cyan")
     for item in lst:
         print(str(item[0]) + ". \"" + str(item[1] + "\""))
         counter += 1
+    print()
+
+
+def create_enum_directories(cwd):
+    dir_list = []
+    for directory in os.listdir(cwd):
+        if os.path.isdir(directory):
+            dir_list.append(directory)
+        else:
+            continue
+
+    for i, d in enumerate(dir_list):
+        dir_list[i] = [i, d]
+
+    return dir_list
 
 
 def set_dir():
@@ -20,39 +36,114 @@ def set_dir():
         pick = pick.upper()
 
         if pick == "Y":
-            path = input("Enter a path:")
+            path = input("Enter a path: ")
             os.chdir(path)
             cwd = os.getcwd()
-            break
+            return cwd
         elif pick == "N":
-            break
+            return cwd
         else:
             continue
 
-    dir_list = []
-    for directory in os.listdir(cwd):
-        if os.path.isdir(directory):
-            dir_list.append(directory)
+
+def print_jpgs(cwd):
+    cprint("JPGs:", "cyan")
+    counter = 0
+    jpg_list = os.listdir(cwd)
+    for item in jpg_list:
+        if item.endswith(".jpg"):
+            print(str(counter) + ". " + item)
+            counter += 1
+
+    if counter > 0:
+        cprint("\nWould you like to convert all of the JPGs to PNGs and put them in the \"new\" directory?", "magenta")
+        while True:
+            pick = str(input("Y/N: "))
+            pick = pick.upper()
+            if pick == "Y":
+                if os.path.exists(cwd + "\\new"):
+                    try:
+                        new_dir_path = cwd + "\\new"
+                        for img in jpg_list:
+                            img_pick = Image.open(cwd + "\\" + img)
+                            img_pick.save(new_dir_path + "\\" + img[:-4] + ".png")
+                    except PermissionError as e:
+                        cprint("No permission! Stopping process.", "red")
+                    break
+                else:
+                    while True:
+                        try:
+                            new_dir_path = cwd + "\\new"
+                            os.mkdir(new_dir_path)
+                            for img in jpg_list:
+                                img_pick = Image.open(cwd + "\\" + img)
+                                img_pick.convert('RGB')
+                                img_pick.close()
+                                img_pick = Image.open(cwd + "\\" + img)
+                                img_pick.save(new_dir_path + "\\" + img[:-4] + ".png")
+                            break
+                        except PermissionError as e:
+                            cprint("No permission! Stopping process.", "red")
+                            continue
+                        break
+            elif pick == "N":
+                break
+            else:
+                continue
+    else:
+        cprint("NO JPGs FOUND\n", "yellow")
+
+
+def choose_next_dir(cwd, dir_list):
+    try:
+        print_enumerated_list(dir_list)
+        print_jpgs(cwd)
+        pick = int(input("Enter the next directory's number: "))
+        dir_num = len(dir_list)
+        if pick >= dir_num or pick < 0:
+            cprint("NOT IN RANGE\n", "red")
+            choose_next_dir(cwd, dir_list)
         else:
-            continue
+            for num in dir_list:
+                if num[0] == pick:
+                    next_dir = "\\" + str(num[1])
+                    cwd = cwd + next_dir
+                    dir_list = create_enum_directories(cwd)
+                    break
 
-    for i, d in enumerate(dir_list):
-        dir_list[i] = [i, d]
+            if not dir_list:
+                print_jpgs(cwd)
 
-    print_enumerated_list(dir_list)
-
-
-set_dir()
+            else:
+                choose_next_dir(cwd, dir_list)
 
 
-# cwd = os.getcwd()
-# next_dir = "\\Pokedex"
-# check_dir = "\\new"
 
-# while True:
-#     if os.path.exists(cwd + next_dir + check_dir):
-#         for img in os.listdir(cwd + next_dir):
-#             print(img)
-#         break
-#     else:
-#         os.mkdir(cwd + next_dir + check_dir)
+    except ValueError as e:
+        print("Not a number.")
+
+
+def main():
+    cwd = set_dir()
+    if os.path.exists(cwd + "\\new"):
+        dir_list = create_enum_directories(cwd)
+        choose_next_dir(cwd, dir_list)
+
+    else:
+        print("Would you like to create a file named \"new\"?")
+        while True:
+            pick = str(input("Y/N: "))
+            pick = pick.upper()
+            if pick == "Y":
+                os.mkdir(cwd + "\\new")
+                break
+            elif pick == "N":
+                break
+            else:
+                continue
+        dir_list = create_enum_directories(cwd)
+        choose_next_dir(cwd, dir_list)
+
+
+if __name__ == '__main__':
+    main()
